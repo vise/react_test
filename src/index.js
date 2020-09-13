e4c4e01eecbc98bcf42be085d0392207001e0673
Viggo Stenseth <vstenseth@spotify.com>
2020-09-13 21:45:08 +0200

    input

--- src/index.js
+++ src/index.js
@@ -2,29 +2,28 @@ import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Checkbox() {
-  const [checked, setChecked] = useState(false);
-  useEffect(() => {
-    alert(`checked: ${checked.toString()}`)
-  })
-  return(
-    <>
-      <input 
-        type="checkbox" 
-        alue={checked} 
-        onChange={() =>
-          setChecked(checked => !checked)}
-      />
-      {checked ? "checked" : "not checked"}
-    </>
-  );
-}
-
 function App(){
+  const [val, setVal] = useState("");
+  const [val2, setVal2] = useState("");
 
+  useEffect(() => {
+    console.log(`field1: ${val}`);
+  }, [val])
+  
+  return (
+    <>
+      <label>
+        Input 1:
+        <input 
+          value={val} 
+          onChange={e => setVal(e.target.value)} 
+        />
+      </label>
+    </>
+  )
 }
 
 ReactDOM.render(
-  <Checkbox />,
+  <App />,
   document.getElementById('root')
 );
